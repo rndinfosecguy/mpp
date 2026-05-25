3f4a5da1cd90c2064fb84fbe28aed62c361990d1
rndinfosecguy <hy3n4@protonmail.com>
2026-05-25 08:05:33 +0200

    change: show_dns_traffic fine tuning

--- show_dns_traffic/payload.sh
+++ show_dns_traffic/payload.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+# Name: show dns traffic
+# Description: shows the traffic captured in a previous dns capture.
+# Author: Hy3n4
+# Version: 1.0
+# Category: interception
 
-PAYLOAD_DIR="/root/payloads/user/mpp/show_dns_traffic"
+export PATH="/mmc/usr/bin:${_PAYLOAD_HOME}/bin:$PATH"
+export PYTHONPATH="${_PAYLOAD_HOME}/lib:${_PAYLOAD_HOME}:$PYTHONPATH"
+export LD_LIBRARY_PATH="/mmc/usr/lib:${_PAYLOAD_HOME}/lib:$LD_LIBRARY_PATH"
 
-export PATH="/mmc/usr/bin:$PAYLOAD_DIR/bin:$PATH"
-export PYTHONPATH="$PAYLOAD_DIR/lib:$PAYLOAD_DIR:$PYTHONPATH"
-export LD_LIBRARY_PATH="/mmc/usr/lib:$PAYLOAD_DIR/lib:$LD_LIBRARY_PATH"
+if [ ! -d "/mmc/root/loot/mpp/" ]; then
+  ERROR_DIALOG "Error: /mmc/root/loot/mpp/ does not exist."
+  exit 0
+fi
+
+pcap_files=($(find /mmc/root/loot/mpp/ -type f -name "*.pcap" -print))
+
+if [ ${#pcap_files[@]} -eq 0 ]; then
+  ERROR_DIALOG "No pcap files found in /mmc/root/loot/mpp/."
+  exit 0
+fi
 
 options=()
 
-for file in /mmc/root/loot/mpp/*; do
-    [ -f "$file" ] || continue
+for file in "${pcap_files[@]}"; do
+    if [[ "$file" =~ \.pcap$ ]]; then
     options+=("$(basename "$file")")
+    fi
 done
 
 # Pick first file as default
@@ -18,7 +34,8 @@ default="${options[0]}"
 
 selection=$(LIST_PICKER "Choose Payload" "${options[@]}" "$default") || exit 0
 LOG "Selected: $selection"
-LOG "--------------------"
+LOG "It may take some time depending on the file size."
+LOG "-------------------------------------------------"
 
 res=$(python3 analyze.py /mmc/root/loot/mpp/"$selection")
 LOG "$res"
